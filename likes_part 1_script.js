1eb8bc55141aba8346f8818667f4d3ae86853bc5
nkchambers <nkchames@gmail.com>
2023-08-22 23:45:03 -0700

    adding Likes for 8-22-23

--- likes_part 1_script.js
+++ likes_part 1_script.js
@@ -1,9 +1,9 @@
 var count = 3;
-var su_likesElement = document.querySelector ("#su_likes");
+var sue_likesElement = document.querySelector ("#su_likes");
 
 
 function addLikes() {
     count++;
-    su_likesElement.innerText = count + "like(s)";
+    sue_likesElement.innerText = count + "like(s)";
     console.log (count);
 }
